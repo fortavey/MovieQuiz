9f6a68727f2ecd0a8a4dc8c346d13569d7baf5ef
Anton Shapoval <antonshapoval@MacBook-Air-Anton.local>
2024-12-23 23:52:19 -0800

    Finish

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,25 +1,189 @@
 import UIKit
 
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
+
+struct QuizStepViewModel {
+  let image: UIImage
+  let question: String
+  let questionNumber: String
+}
+
+struct QuizResultsViewModel {
+  let title: String
+  let text: String
+  let buttonText: String
+}
+
 final class MovieQuizViewController: UIViewController {
-    // MARK: - Lifecycle
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true
+        ),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true
+        ),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true
+        ),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true
+        ),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true
+        ),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true
+        ),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false
+        ),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false
+        ),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false
+        ),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false
+        ),
+    ]
+
+    @IBOutlet private var questionWord: UILabel!
+    @IBOutlet private var counterLabel: UILabel!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var imageView: UIImageView!
+    
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Присвоение правильных шрифтов для Label (косяк Xcode16)
+        initFonts()
+        
+        // Отрисовка первого вопроса
+        show(quiz: convert(model: questions[currentQuestionIndex]))
+        
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
     }
+    
+    private func initFonts() {
+        questionWord.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        counterLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        textLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        counterLabel.text = step.questionNumber
+        textLabel.text = step.question
+        imageView.image = step.image
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)"
+        )
+    }
+    
+    
+    private func show(quiz result: QuizResultsViewModel) -> UIAlertController {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            self.hideBorder()
+            self.show(quiz: self.convert(model: self.questions[self.currentQuestionIndex]))
+        }
+        alert.addAction(action)
+        
+        return alert
+    }
+    
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            self.present(show(quiz: QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: "Ваш результат \(correctAnswers)/\(questions.count)",
+                buttonText: "Сыграть ещё раз"
+            )), animated: true, completion: nil)
+        } else {
+            currentQuestionIndex += 1
+            show(quiz: convert(model: questions[currentQuestionIndex]))
+            hideBorder()
+        }
+    }
+    
+    private func hideBorder() {
+        imageView.layer.borderWidth = 0
+    }
+    
+    private func changeBorder(isCorrect: Bool) {
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+    }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
+        changeBorder(isCorrect: isCorrect)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResults()
+        }
+    }
+    
+    private func userAnsverHandler(userAnsver: Bool) {
+        let correctAnsver: Bool = questions[currentQuestionIndex].correctAnswer
+        showAnswerResult(isCorrect: correctAnsver == userAnsver)
+    }
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        userAnsverHandler(userAnsver: true)
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        userAnsverHandler(userAnsver: false)
+    }
+    
 }
 
 /*
- Mock-данные
- 
- 
- Картинка: The Godfather
  Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Dark Knight
  Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: ДА
  
  
